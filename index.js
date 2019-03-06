0f69dabf1e6bf90a6ec5bfc5c96797a2fab41d00
oz <ozfraier@gmail.com>
2019-03-06 18:37:00 +0200

    modifying index.js to use express and oas-tools, see https://github.com/swagger-api/swagger-codegen/issues/9213

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-'use strict';
+"use strict";
 
-var fs = require('fs'),
-    path = require('path'),
-    http = require('http');
+var fs = require("fs"),
+  path = require("path"),
+  http = require("http");
 
-var app = require('connect')();
-var oas3Tools = require('oas3-tools');
-var jsyaml = require('js-yaml');
+var jsyaml = require("js-yaml");
 var serverPort = 8080;
 
 // swaggerRouter configuration
 var options = {
-  swaggerUi: path.join(__dirname, '/swagger.json'),
-  controllers: path.join(__dirname, './controllers'),
-  useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
+  swaggerUi: path.join(__dirname, "/swagger.json"),
+  controllers: path.join(__dirname, "./controllers"),
+  useStubs: process.env.NODE_ENV === "development" // Conditionally turn on stubs (mock mode)
 };
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var spec = fs.readFileSync(path.join(__dirname, "api/swagger.yaml"), "utf8");
 var swaggerDoc = jsyaml.safeLoad(spec);
-
-// Initialize the Swagger middleware
-oas3Tools.initializeMiddleware(swaggerDoc, function (middleware) {
-
+var express = require("express");
+var app = express();
+var oasTools = require("oas-tools");
+oasTools.initializeMiddleware(swaggerDoc, app, function(middleware) {
   // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
   app.use(middleware.swaggerMetadata());
 
@@ -36,9 +34,15 @@ oas3Tools.initializeMiddleware(swaggerDoc, function (middleware) {
   app.use(middleware.swaggerUi());
 
   // Start the server
-  http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  http.createServer(app).listen(serverPort, function() {
+    console.log(
+      "Your server is listening on port %d (http://localhost:%d)",
+      serverPort,
+      serverPort
+    );
+    console.log(
+      "Swagger-ui is available on http://localhost:%d/docs",
+      serverPort
+    );
   });
-
 });
