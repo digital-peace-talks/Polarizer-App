bdd406fc8a5d0dddb81d89c3e056177efd59d2c3
Aviran Katz <aviran@soficoop.com>
2019-04-02 17:00:28 +0300

    CORS enabled

--- src/api/index.js
+++ src/api/index.js
@@ -1,37 +1,42 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const config = require('../lib/config');
-const logger = require('../lib/logger');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const config = require("../lib/config");
+const logger = require("../lib/logger");
+const cors = require("cors");
 
 const log = logger(config.logger);
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors);
 
 /*
  * Routes
  */
-app.use('/metadata', require('./routes/metadata'));
-app.use('/user', require('./routes/user'));
-app.use('/statement', require('./routes/statement'));
-app.use('/dialog', require('./routes/dialog'));
+app.use("/metadata", require("./routes/metadata"));
+app.use("/user", require("./routes/user"));
+app.use("/statement", require("./routes/statement"));
+app.use("/dialog", require("./routes/dialog"));
 
 // catch 404
 app.use((req, res, next) => {
   log.error(`Error 404 on ${req.url}.`);
-  res.status(404).send({status: 404, error: 'Not found'});
+  res.status(404).send({ status: 404, error: "Not found" });
 });
 
 // catch errors
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const msg = err.error || err.message;
-  log.error(`Error ${status} (${msg}) on ${req.method} ${req.url} with payload ${req.body}.`);
-  res.status(status).send({status, error: msg});
+  log.error(
+    `Error ${status} (${msg}) on ${req.method} ${req.url} with payload ${
+      req.body
+    }.`
+  );
+  res.status(status).send({ status, error: msg });
 });
-
 
 module.exports = app;
