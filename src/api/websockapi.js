6743db95b6af65b3764358428685d12ae4183c57
thomax <thomax@dpt.world>
2019-07-27 21:26:58 +0200

    needs more refinement. but, hey.

---
+++ src/api/websockapi.js
@@ -0,0 +1,17 @@
+//Normal code here
+
+//then at the bottom:
+
+module.exports = function (io) {
+    //Socket.IO
+    io.on('connection', function (socket) {
+        console.log('User has connected to Index');
+        //ON Events
+        socket.on('admin', function () {
+            console.log('Successful Socket Test');
+        });
+
+        //End ON Events
+    });
+    return router;
+};
