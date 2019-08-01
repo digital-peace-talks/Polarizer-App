1d7599b3b83805c2033ef51cb4d68361afa7dbef
thomax <thomax@dpt.world>
2019-08-01 21:21:37 +0200

    webhook integrated. unsecured, yet. but who cares what we right now.

--- src/api/routes/misc.js
+++ src/api/routes/misc.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const metadata = require('../services/metadata');
+const config		= require("../../lib/config");
+const logger	= require("../../lib/logger");
+const log		= logger(config.logger);
 
 const router = new express.Router();
 
@@ -13,6 +16,34 @@ router.get('/api', async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+});
+
+router.post('/webhook', (req, res) => {
+    // maybe we should spawn a external shell script
+    // to keep this code clean and gain flexibility.
+    // instead we should have a look at the req body.
+
+    const { spawnSync } = require('child_process');                                 
+    var context;
+if(0) {
+    router.io.emit('update', {
+        method: 'post',
+        path: '/info',
+        payload: {
+            message: 'Update info: A new version got delivered. The server will restart very soon.'
+        }
+    });
+}
+    console.log('got this:'+JSON.stringify(req.body));     
+    res.status(201).send('take me off.');
+
+    log.info('run git pull');         
+    context = spawnSync('git', ['pull']);          
+    log.info(context.stdout.toString());                          
+
+    log.info('run npm install');
+    context = spawnSync('npm', ['install']);
+    log.info(context.stdout.toString());                      
 });
 
 module.exports = router;
