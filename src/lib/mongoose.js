f5fffa001501fa3636bf2da4968045872bd347b5
Metin74 <metin@digitalpeacetalks.com>
2019-08-14 12:04:54 +0200

    info to logger output when mongodb unreachable

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 const config = require("../lib/config");
+
+const logger	= require("../lib/logger");
+const log		= logger(config.logger);
+
 const dbUrl = config.dbUrl;
+// Connect to the server
+
+
 const connection = mongoose.connect(dbUrl, {
   useNewUrlParser: true,
+}, function(error) {
+  if(error) {
+    log.error("db error, can't connect " + error.message);
+  }
 });
+
 exports.db = mongoose.connection;
