fc171f9f63c13099346790da2464ad98e554238e
Martin <martin@dpt.world>
2019-08-19 16:35:19 +0200

    new babylon.html and changed frontend.js

--- src/api/routes/frontend.js
+++ src/api/routes/frontend.js
@@ -129,4 +129,35 @@ router.get('/dialog', async (req, res, next) => {
     }
 });
 
+//Deliver the babylon page
+router.get('/babylon', async (req, res, next) => {
+	try {
+		// Do we need the 'real' IP address?
+		// var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+		var dptUUID = uuid();
+		var cookieOptions = {
+			maxAge: 31536000000, // 1000 * 60 * 60 * 24 * 365 ===> Valid for one year
+			signed: true,
+			httpOnly: false
+		}
+
+		// First time visitor?
+		if(req.signedCookies.dptUUID === undefined) {
+			console.log("no cookie found, set new one");
+			// The client need to get the uuid for the first time, it needs to send it back.
+			cookieOptions.httpOnly = false;
+			res.cookie('dptUUID', dptUUID, cookieOptions)
+		} else {
+			console.log("use old cookie");
+			dptUUID = req.signedCookies.dptUUID;
+			res.cookie('dptUUID', dptUUID, cookieOptions)
+		}
+		console.log('session tagged with dptUUID '+dptUUID);
+		res.sendFile(process.env.DPT_PATH+'/static/babylon.html');
+		res.end;
+   	} catch(err) {
+   		next(err);
+    }
+});
+
 module.exports = router;
