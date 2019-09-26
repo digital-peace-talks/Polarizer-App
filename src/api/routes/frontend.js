c64dee299ed008ce471a429f5ff23f1f8be30a2f
Martin <martin@dpt.world>
2019-09-26 17:31:28 +0200

    some frontside stuff

--- src/api/routes/frontend.js
+++ src/api/routes/frontend.js
@@ -8,23 +8,22 @@ const getPhrase = require('../../lib/phrasegenerator')
 
 const router = new express.Router();
 
+
 router.get('/', async(req, res, next) => {
     try {
         if (req.signedCookies.dptUUID === undefined) {
             var phrase = await getPhrase();
             console.log("no cookie found, set new one");
             // The client need to get the uuid for the first time, it needs to send it back.
-            res.send('<center><img src="https://www.digitalpeacetalks.com/img/DPT_Logo_Ball_blue.png" alt="digital peace talks" height="400" width="400"><br>you are a new user?<br><br>thats your phrase:<br>' + phrase +
+            res.send('<body bgcolor="#0071bc"><center><img src="https://www.digitalpeacetalks.com/img/DPT_Logo_Ball_blue.png" alt="digital peace talks" height="400" width="400"><br>you are a new user?<br><br>thats your phrase:<br>' + phrase +
                 '<br><br>you are user on another browser?<br><br>enter your pass phrase<br>' +
                 '<form method="post" action="/recover"><input type=text name=phraseinput>' +
                 '<input type="hidden" name="phrase" value="' + phrase + '"></form></center>');
         } else {
-            res.send(`<center>
-            <img src="https://www.digitalpeacetalks.com/img/DPT_Logo_Ball_blue.png" alt="digital peace talks" height="400" width="400">
-            <br><br><a href=/launch>start dpt protype</a><br><br>
-					<a href=/babylon.html>babylon sample</a><br><br>
-					<a href=/launch3d.html>launch3D sample</a><br><br>
-					<a href=/dialog.html>dialog sample</a><br><br>
+            res.send(`<body bgcolor="#0071bc"><center><img src="https://www.digitalpeacetalks.com/img/DPT_Logo_Ball_blue.png" alt="digital peace talks" height="400" width="400">
+            <br><br> 
+					<a href=/launch3d.html>Start</a><br><br>
+				
 					<!--
 					<a onClick="function gcv(a) {var b=document.cookie.match('(^|;)\\s*'+a+'\\s*=\\s*([^;]+)');return b?b.pop():''};document.cookie='dptUUID='+gcv('dptUUID')+'; max-age=0; path=/; domain='+window.location.hostname+';location.reload(true);">delete cookie</a>
 					-->
