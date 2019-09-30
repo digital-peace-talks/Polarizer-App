9e79328328173096d4c259e929bb5711d1d3206f
thomax <thomax@dpt.world>
2019-09-30 10:57:39 +0200

    start link (recover get) on start page.

--- src/api/routes/frontend.js
+++ src/api/routes/frontend.js
@@ -14,14 +14,16 @@ router.get('/', async(req, res, next) => {
         if (req.signedCookies.dptUUID === undefined) {
             var phrase = await getPhrase();
             console.log("no cookie found, set new one");
+            console.log("new phrase: "+phrase);
             // The client need to get the uuid for the first time, it needs to send it back.
             res.send(`<body bgcolor="#0071bc"><center><img src="https://www.digitalpeacetalks.com/img/DPT_Logo_Ball_blue.png" alt="digital peace talks" height="400" width="400">
                 <br>Are you a new user?<br><br>
                 <fieldset style="text-align:center; width:400px">
-                <legend style="text-align:center">This could be your pass-phrase:</legend>
+                <legend style="text-align:center">This could be your pass-phrase, remember it:</legend>
                 <h3><b>${phrase}</b>
+                <br><a href=/recover?phrase=${encodeURIComponent(phrase)}>Start</a>
                 </fieldset>
-                </h3><br><br>Are you also on another browser?<br><br>Enter your pass-phrase:<br>
+                </h3><br><br>Lost your cookie? A new browser? Recover here.<br><br>Enter your pass-phrase:<br>
                 <form method="post" action="/recover"><input type=text name=phraseinput>
                 <input type="hidden" name="phrase" value="${phrase}"></form></center>`);
         } else {
@@ -43,6 +45,38 @@ router.get('/', async(req, res, next) => {
         res.status(200);
     } catch (err) {
         next(err);
+    }
+});
+
+router.get('/recover', async(req, res, next) => {
+	console.log('recover get ' + req.query.phrase);
+    var dptUUID;
+    var cookieOptions = {
+        maxAge: 31536000000, // 1000 * 60 * 60 * 24 * 365 ===> Valid for one year
+        signed: true,
+        httpOnly: false
+    }
+    if (req.signedCookies.dptUUID === undefined) {
+        console.log("no cookie found, set new one");
+        // The client need to get the uuid for the first time, it needs to send it back.
+        //		cookieOptions.httpOnly = false;
+        //		res.cookie('dptUUID', dptUUID, cookieOptions)
+    } else {
+        console.log("use old cookie");
+        dptUUID = cookieParser.signedCookie(req.signedCookies.dptUUID, process.env.DPT_SECRET);
+    }
+    var ret = await userService.userReclaim({ body: { phraseGuess: '', newPhrase: req.query.phrase, dptUUID: dptUUID } });
+    if (ret.newCookie) {
+        res.cookie('dptUUID', ret.newCookie, cookieOptions);
+        await res.writeHead(302, {
+            'Location': '/launch3d.html'
+                //'Location': '/'
+                //add other headers here...
+        });
+        res.end();
+    } else {
+        res.send('bla ' + req.body.phraseinput);
+        res.status(201);
     }
 });
 
