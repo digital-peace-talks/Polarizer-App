ebd05c86d84006b8c5a24f4a6ac1de8d999004ae
thomax <thomax@dpt.world>
2019-10-11 11:05:52 +0200

    remove leftovers.

--- html/babylon-topics.js
+++ html/babylon-topics.js
@@ -5,9 +5,6 @@ function loadTopics(restObj) {
 
 	var xstart = -cols*4.8/2 + 4.8/2;
 	var ystart = cols*3.2/2 + 3.2/2;
-	console.log(`xstart: ${xstart} - ystart ${ystart}`);
-var box = new BABYLON.MeshBuilder.CreateBox("box", {width: 1, height: 1, depth: 1,}, currentScene);
-box.position = new BABYLON.Vector3(0,0,0);
 
 	if(currentScene.name == 'topicScene') {
 		for(var i = currentScene.meshes.length - 1; i >= 0; i--) {
