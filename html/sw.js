480631d30d070ca06c0fe5c1f0a2ca45086d8d00
Metin74 <metin@digitalpeacetalks.com>
2019-10-23 14:29:27 +0200

    cleared cache-folder in serviceWorker

--- html/sw.js
+++ html/sw.js
@@ -1,30 +1,6 @@
 var cacheName = 'dpt-v1';
 const filesToCache = [
-  '/',
-  '/dpt3d.html',
-  'babylon-main.js',
-  'babylon-misc.js',
-  'babylon-opinions.js',
-  'babylon-topics.js',
-  'dpt3d.html',
-  'dpt3d.js',
-  'dialog.html',
-  'dialog.js',
-  'dpt-client.js',
-  'dpt_start.css',
-  'dpt_classic.css',
-  'dpt_classic.css.map',
-  'dpt_classic.scss',
-  'html-forms.js',
-  'icons/icon-192x192.png',
-  'icons/icon-512x512.png',
-  'dpt_logo.png',
-  '/search.png',
-  'https://code.jquery.com/pep/0.4.3/pep.js',
-  'https://preview.babylonjs.com/babylon.js',
-  'https://preview.babylonjs.com/materialsLibrary/babylonjs.materials.min.js',
-  'https://preview.babylonjs.com/serializers/babylonjs.serializers.min.js',
-  'https://preview.babylonjs.com/gui/babylon.gui.min.js'
+  '/'
 ];
 
 // install event
