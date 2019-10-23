5c2b114ad460471fe8aa2ec65e6641730c7c7588
thomax <thomax@dpt.world>
2019-10-23 16:49:41 +0200

    test prevent caching start page.

--- html/sw.js
+++ html/sw.js
@@ -1,29 +1,35 @@
 var cacheName = 'dpt-v1';
 const filesToCache = [
-  '/'
+	'/'
 ];
 
 // install event
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
+	console.log('[ServiceWorker] Install');
+	e.waitUntil(
+		caches.open(cacheName)
+		.then(function(cache) {
+			console.log('[ServiceWorker] Caching app shell');
+			return cache.addAll(filesToCache);
+		})
+	);
 });
 
 // activate event
 self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
+	event.waitUntil(self.clients.claim());
 });
 
 // fetch event
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+	event.respondWith(
+		caches.match(event.request, {ignoreSearch: true})
+		.then(response => {
+			if(event.request.url == "https://proto1.dpt.world/") {
+				return(fetch(event.request));
+			} else {
+				return(response || fetch(event.request));
+			}
+		})
+	);
 });
