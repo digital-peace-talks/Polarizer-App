89669b3f66717ee1edf928d47d014e81a062a02e
patrickmoy <patrickrnmoy@gmail.com>
2021-01-21 15:22:02 -0800

    Bunyan heroku bugfix v3

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -28,5 +28,5 @@ module.exports = config => {
     bunyanConfig.push(logger);
   });
 
-  return bunyan.createLogger({ src: true, name: "dpt", streams: bunyanConfig });
+  return bunyan.createLogger({ src: true, name: config.name, streams: bunyanConfig });
 };
