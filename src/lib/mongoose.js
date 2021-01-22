9eeb3764217ff018e25082448ab838ab8a8ab0d5
patrickmoy <patrickrnmoy@gmail.com>
2021-01-21 18:21:50 -0800

    Reconfigure to use heroku config var as first option for mongo access

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -4,7 +4,7 @@ const config = require("../lib/config");
 const logger	= require("../lib/logger");
 const log		= logger(config.logger);
 
-const dbUrl = config.dbUrl;
+const dbUrl = process.env.MONGODB_URI || config.dbUrl;
 // Connect to the server
 
 
