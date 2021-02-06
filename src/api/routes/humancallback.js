f95153bc08252db20e025daa294cdf1721f05549
patrickmoy <patrickrnmoy@gmail.com>
2021-02-06 12:36:29 -0800

    Log output for debug exchange call

--- src/api/routes/humancallback.js
+++ src/api/routes/humancallback.js
@@ -16,18 +16,16 @@ router.get("/", async (req, res, next) => {
   }
 
   const encoded = req.query.et;
+  console.log(encoded);
   try {
     const exchange = decodeURIComponent(req.query.et);
+    console.log(exchange);
     fetch('https://core.human-id.org/v0.0.3/server/users/exchange', {
       method: 'post',
       headers: { 'client-id': process.env.DPT_HUMAN_ID, 'client-secret': process.env.DPT_HUMAN_SECRET, 'Content-Type': 'application/json' },
       body: { "exchangeToken": exchange }
     })
-      .then(res => res.json())
-      .then((json) => {
-        console.log(json);
-        // res.status(301).redirect(json.data.webLoginUrl)
-      })
+      .then(res => console.log(res))
       .catch(err => console.log("Unable to reach humanID server: " + err))
 
   } catch (err) {
